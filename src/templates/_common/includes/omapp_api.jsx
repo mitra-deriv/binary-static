8a4aa0ea2e209a2ccf42688bbfabce09e39d0f38
mitra-fs <mitra@firstsource.tech>
2022-02-17 16:04:45 +0330

    fix for test failures

--- src/templates/_common/includes/omapp_api.jsx
+++ src/templates/_common/includes/omapp_api.jsx
@@ -1,11 +1,10 @@
-import React from "react";
+import React from 'react';
 
 const OmappApi = () => (
-  <React.Fragment>
-    <script
-      type="text/javascript"
-      dangerouslySetInnerHTML={{
-        __html: `
+    <React.Fragment>
+        <script
+            dangerouslySetInnerHTML={{
+                __html: `
             (function (d, u, ac) {
                 var s = d.createElement("script");
                 s.type = "text/javascript";
@@ -16,9 +15,9 @@ const OmappApi = () => (
                 d.getElementsByTagName("head")[0].appendChild(s);
               })(document, 163734, 177733); 
             `,
-      }}
-    />
-  </React.Fragment>
+            }}
+        />
+    </React.Fragment>
 );
 
 export default OmappApi;
